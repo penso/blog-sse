82f245c7f8dc353eb3336372230b88fb8cc84fc3
Fabien Penso <fabienpenso@gmail.com>
2013-05-23 21:51:14 +0200

    RealTime analytics working, with random JS data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,69 @@
 //
 //= require highcharts
 //= require highcharts/highcharts-more
+
+$(function() {
+  // Create the chart
+  $('#container').highcharts({
+    chart: {
+      type: 'area',
+      plotBorderWidth: 0,
+      events: {
+        load: function() {
+          // set up the updating of the chart each second
+          var series = this.series[0];
+          setInterval(function() {
+            var x = (new Date()).getTime(), // current time
+
+            // Previous value +/- 10 max
+            y = Math.round(series.data.slice(-1)[0]['y'] + (Math.random() * 10 * (Math.round(Math.random()) * 2 - 1)));
+            if (y < 0) {
+              y = -y
+            }
+            series.addPoint([x, y], true, true);
+          }, 1000);
+        }
+      }
+    },
+    title: {
+      text: 'Live random data'
+    },
+    xAxis: {
+      type: 'datetime'
+    },
+    yAxis: {
+      gridLineWidth: 0.3,
+      title: {text: ""}
+    },
+    plotOptions: {
+      area: {
+        fillOpacity: 0.35,
+        states: {
+          hover: {
+            enabled: false
+          }
+        },
+        marker: {
+          enabled: true,
+          symbol: 'circle',
+          radius: 2
+        }
+      }
+    },
+    legend: { enabled: false },
+    credits: { enabled: false },
+    series: [{
+      name: 'Random data',
+      lineWidth: 2,
+      data: (function() {
+        // generate an array of random data
+        var data = [], time = (new Date()).getTime(), i;
+        var previous;
+        for( i = -60; i <= 0; i+=1) {
+          data.push([time + i * 1000, Math.round(Math.random() * 100)]);
+        }
+        return data;
+      })()
+    }]
+  });
+});
---
+++ app/controllers/analytics_controller.rb
@@ -0,0 +1,5 @@
+class AnalyticsController < ApplicationController
+  def index
+
+  end
+end
