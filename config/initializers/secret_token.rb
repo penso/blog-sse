e4d1e414297797d42874b810d38a722d120c658b
Fabien Penso <fabienpenso@gmail.com>
2013-05-23 21:23:10 +0200

    1st commit with Gemfile
    This is a rails example for realtime data update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogSse::Application.config.secret_token = '585d4c7efb3286824620e2b5f3441c018d63b3c2e2f47ba5778199b2a92ffe7c44ca8ebb8eacd619deb7e65a51a4495c5ddfe25e7d6ec8249cb33881fe2ea0a6'
